1db3d7264bafda6a1ad1cbefbd57d24d15bb5954
Franck Benoit <fbenoit@lamobilery.fr>
2024-03-26 09:23:27 +0100

    fix: adapt error from sprint to fatal encode test

--- rpc/rpc_test.go
+++ rpc/rpc_test.go
@@ -13,6 +13,8 @@ func TestEncode(t *testing.T) {
 	expected := "Content-length: 16\r\n\r\n{\"Testing\":true}"
 	actuale := rpc.EncodeMessage(EncodingExample{Testing: true})
 	if expected != actuale {
-		t.Errorf("Expected %s, but got %s", expected, actuale)
+		t.Fatalf("Expected %s, but got %s", expected, actuale)
+	}
+}
 	}
 }
